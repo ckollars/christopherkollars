1f58c9897fd65639f2ca6265035f4d623f7e5ca4
Christopher Kollars <chris@christopherkollars.com>
2016-09-05 14:22:10 -0700

    Wow more changes to gulp, jekyll and the build process. I think I finally have it

--- gulpfile.js
+++ gulpfile.js
@@ -7,57 +7,51 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
+    bourbon = require('bourbon'),
+    neat = require('bourbon-neat'),
     cp = require('child_process');
 
 var messages = {
-  jekyllDev: 'Running: $ jekyll build for dev',
-  jekyllProd: 'Running: $ jekyll build for prod'
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
 /**
- * Jekyll Site - Development build
+ * Build the Jekyll Site
  */
-gulp.task('jekyll-dev', function (done) {
-  browserSync.notify(messages.jekyllDev);
-  return cp.spawn('jekyll', ['build', '--drafts', '--config', '_config.yml'], {stdio: 'inherit'})
- .on('close', done);
-});
-
-/**
- * Jekyll Site - Production build
- */
-gulp.task('jekyll-prod', function (done) {
-  browserSync.notify(messages.jekyllProd);
-  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-  .on('close', done);
+gulp.task('jekyll-build', function (done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn( 'jekyll' , ['build'], {stdio: 'inherit'})
+    .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-dev'], function () {
-  browserSync.reload();
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['css', 'scripts', 'jekyll-dev'], function() {
-  browserSync.init({
-    server: "_site",
+gulp.task('browser-sync', ['css', 'jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    },
     port: 3000
   });
 });
 
+
 /**
  * CSS
  */
-
 gulp.task('css', function () {
-    return gulp.src('./_scss/styles.scss')
+    return gulp.src('./assets/scss/styles.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
-        includePaths: ['scss'],
+        includePaths: ['bourbon', 'bourbon-neat', 'scss'],
         onError: browserSync.notify
     }))
     .pipe(autoprefixer({
@@ -71,8 +65,9 @@ gulp.task('css', function () {
     .pipe(cssnano({processImport: false}))
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./_site/css/'))
+    .pipe(gulp.dest('./_site/assets/css/'))
     .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('assets/css'));
 });
 
 /**
@@ -95,14 +90,15 @@ gulp.task('scripts', function(){
 });
 
 
+
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-  gulp.watch(['_scss/**/*.scss','_scss/*.scss'], ['css']);
-  gulp.watch(['_js/**/*.js'], ['scripts']);
-  gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*.html', '_drafts/*', '**/*.html'], ['jekyll-rebuild']);
+    gulp.watch('_scss/*.scss', ['css']);
+    gulp.watch('js/*.js', ['scripts']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
@@ -110,9 +106,6 @@ gulp.task('watch', function () {
  * compile the jekyll site, launch BrowserSync & watch files.
  */
 gulp.task('default', ['browser-sync', 'watch']);
-
-gulp.task('build', ['scripts', 'css', 'jekyll-prod']);
-
 
 // Handle errors
 function handleError (error) {
