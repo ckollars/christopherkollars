17df49f7aa5e8019c4a2675cad64468c11af3bf4
Chris Kollars <chris.kollars@aviture.us.com>
2024-01-03 21:12:00 -0600

    chore: update 11ty config to match sass and settings to compile sass to css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,30 +1,32 @@
-const { DateTime } = require("luxon");
-const markdownItAnchor = require("markdown-it-anchor");
+const { DateTime } = require('luxon');
+const markdownItAnchor = require('markdown-it-anchor');
+const eleventySass = require('@11tyrocks/eleventy-plugin-sass-lightningcss');
 
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginBundle = require("@11ty/eleventy-plugin-bundle");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginBundle = require('@11ty/eleventy-plugin-bundle');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
 
 module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		"./src/css": "/css",
-		"./src/img": "/img",
-		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
+		'./src/img': '/img',
+		'./node_modules/prismjs/themes/prism-okaidia.css': '/css/prism-okaidia.css'
 	});
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
 	// Watch content images for the image pipeline.
-	eleventyConfig.addWatchTarget("src/content/**/*.{svg,webp,png,jpeg}");
+	eleventyConfig.addWatchTarget('./src/content/**/*.{svg,webp,png,jpeg}');
+  eleventyConfig.addWatchTarget('./src/css/');
 
 	// App plugins
-	eleventyConfig.addPlugin(require("./eleventy.config.drafts.js"));
-	eleventyConfig.addPlugin(require("./eleventy.config.images.js"));
+	eleventyConfig.addPlugin(require('./eleventy.config.drafts.js'));
+	eleventyConfig.addPlugin(require('./eleventy.config.images.js'));
+  eleventyConfig.addPlugin(eleventySass);
 
 	// Official plugins
 	eleventyConfig.addPlugin(pluginRss);
@@ -36,9 +38,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginBundle);
 
 	// Filters
-	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+	eleventyConfig.addFilter('readableDate', (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+		return DateTime.fromJSDate(dateObj, { zone: zone || 'utc' }).toFormat(format || 'dd LLLL yyyy');
 	});
 
 	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
@@ -47,7 +49,7 @@ module.exports = function(eleventyConfig) {
 	});
 
 	// Get the first `n` elements of a collection.
-	eleventyConfig.addFilter("head", (array, n) => {
+	eleventyConfig.addFilter('head', (array, n) => {
 		if(!Array.isArray(array) || array.length === 0) {
 			return [];
 		}
@@ -59,12 +61,12 @@ module.exports = function(eleventyConfig) {
 	});
 
 	// Return the smallest number argument
-	eleventyConfig.addFilter("min", (...numbers) => {
+	eleventyConfig.addFilter('min', (...numbers) => {
 		return Math.min.apply(null, numbers);
 	});
 
 	// Return all the tags used in a collection
-	eleventyConfig.addFilter("getAllTags", collection => {
+	eleventyConfig.addFilter('getAllTags', collection => {
 		let tagSet = new Set();
 		for(let item of collection) {
 			(item.data.tags || []).forEach(tag => tagSet.add(tag));
@@ -72,21 +74,21 @@ module.exports = function(eleventyConfig) {
 		return Array.from(tagSet);
 	});
 
-	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+	eleventyConfig.addFilter('filterTagList', function filterTagList(tags) {
+		return (tags || []).filter(tag => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1);
 	});
 
 	// Customize Markdown library settings:
-	eleventyConfig.amendLibrary("md", mdLib => {
+	eleventyConfig.amendLibrary('md', mdLib => {
 		mdLib.use(markdownItAnchor, {
 			permalink: markdownItAnchor.permalink.ariaHidden({
-				placement: "after",
-				class: "header-anchor",
-				symbol: "#",
+				placement: 'after',
+				class: 'header-anchor',
+				symbol: '#',
 				ariaHidden: false,
 			}),
 			level: [1,2,3,4],
-			slugify: eleventyConfig.getFilter("slugify")
+			slugify: eleventyConfig.getFilter('slugify')
 		});
 	});
 
@@ -102,24 +104,24 @@ module.exports = function(eleventyConfig) {
 		// Control which files Eleventy will process
 		// e.g.: *.md, *.njk, *.html, *.liquid
 		templateFormats: [
-			"md",
-			"njk",
-			"html",
-			"liquid"
+			'md',
+			'njk',
+			'html',
+			'liquid'
 		],
 
 		// Pre-process *.md files with: (default: `liquid`)
-		markdownTemplateEngine: "njk",
+		markdownTemplateEngine: 'njk',
 
 		// Pre-process *.html files with: (default: `liquid`)
-		htmlTemplateEngine: "njk",
+		htmlTemplateEngine: 'njk',
 
 		// These are all optional:
 		dir: {
-			input: "src/content",         // default: "."
-			includes: "../_includes",  // default: "_includes"
-			data: "../_data",          // default: "_data"
-			output: "_site"
+			input: 'src/content',         // default: "."
+			includes: '../_includes',  // default: "_includes"
+			data: '../_data',          // default: "_data"
+			output: '_site'
 		},
 
 		// -----------------------------------------------------------------
@@ -132,6 +134,6 @@ module.exports = function(eleventyConfig) {
 		// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
 		// it will transform any absolute URLs in your HTML to include this
 		// folder name and does **not** affect where things go in the output folder.
-		pathPrefix: "/",
+		pathPrefix: '/',
 	};
 };
